0f39104b81761b56aa23a6a4e6b7bfc134a45b05
kennethinfante <infantekennethjhim@gmail.com>
2024-11-26 20:48:19 +0800

    Successfully added a table

--- app.py
+++ app.py
@@ -1,52 +1,35 @@
+from dash import Dash, html, Input, Output, dash_table
 import pandas as pd
-import dash
-from dash.dependencies import Input, Output
-from dash import html
-import dash_pivottable
 
-bs_all = pd.read_csv("data-raw/balance_sheet_model.csv")
+bs_all = pd.read_csv('data-raw/balance_sheet_model.csv')
+bs_all = bs_all[bs_all['year'] == 2023]
 
-from data import data
+app = Dash(__name__)
 
-app = dash.Dash(__name__)
-app.title = 'Balance Sheet'
 
 app.layout = html.Div([
-    dash_pivottable.PivotTable(
+    html.H1('Balance Sheet'),
+    html.P(id='balance_sheet'),
+    dash_table.DataTable(
         id='table',
-        data=data,
-        cols=['Day of Week'],
-        colOrder="key_a_to_z",
-        rows=['Party Size'],
-        rowOrder="key_a_to_z",
-        rendererName="Grouped Column Chart",
-        aggregatorName="Average",
-        vals=["Total Bill"],
-        valueFilter={'Day of Week': {'Thursday': False}}
-    ),
-    html.Div(
-        id='output'
-    )
+        columns=[{"name": i, "id": i} 
+                 for i in bs_all.columns],
+        data=bs_all.to_dict('records'),
+        style_cell=dict(textAlign='left'),
+        style_header=dict(backgroundColor="paleturquoise"),
+        style_data=dict(backgroundColor="lavender")
+    ), 
 ])
 
+@app.callback(
+    Output('balance_sheet', 'children'), 
+    Input('table', 'active_cell'))
+    
+def update_graphs(active_cell):
+    if active_cell:
+        cell_data = bs_all.iloc[active_cell['row']][active_cell['column_id']]
+        return f"Data: \"{cell_data}\" from table cell: {active_cell}"
+    return "Click the table"
 
-@app.callback(Output('output', 'children'),
-              [Input('table', 'cols'),
-               Input('table', 'rows'),
-               Input('table', 'rowOrder'),
-               Input('table', 'colOrder'),
-               Input('table', 'aggregatorName'),
-               Input('table', 'rendererName')])
-def display_props(cols, rows, row_order, col_order, aggregator, renderer):
-    return [
-        html.P(str(cols), id='columns'),
-        html.P(str(rows), id='rows'),
-        html.P(str(row_order), id='row_order'),
-        html.P(str(col_order), id='col_order'),
-        html.P(str(aggregator), id='aggregator'),
-        html.P(str(renderer), id='renderer'),
-    ]
-
-
-if __name__ == '__main__':
-    app.run_server(debug=True)
+if __name__ == "__main__":
+    app.run_server(debug=True, port=8051)
